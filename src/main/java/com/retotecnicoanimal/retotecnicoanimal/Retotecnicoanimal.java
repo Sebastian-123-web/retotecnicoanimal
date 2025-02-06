8b6a74123d36248b75a76ecb1a849a1b3cc6ddf2
Sebastian-123-web <sebastian09_26@hotmail.com>
2025-02-06 06:03:33 -0500

    creacion del proyecto

---
+++ src/main/java/com/retotecnicoanimal/retotecnicoanimal/Retotecnicoanimal.java
@@ -0,0 +1,7 @@
+package com.retotecnicoanimal.retotecnicoanimal;
+
+public class Retotecnicoanimal {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
